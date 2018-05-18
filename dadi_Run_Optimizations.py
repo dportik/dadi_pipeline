73a26fbe02ebbc1d84b49c9b04695f24d842beb1
Daniel Portik <daniel.portik@gmail.com>
2018-05-18 10:50:44 -0700

    Update

--- dadi_Run_Optimizations.py
+++ dadi_Run_Optimizations.py
@@ -122,7 +122,7 @@ print "Sum of SFS = ", sfs_sum, '\n', '\n'
     pts: grid size for extrapolation, list of three values
     outfile:  prefix for output naming
     model_name: a label to slap on the output files; ex. "no_mig"
-    func: access the model function from within 'moments_Run_Optimizations.py' or from a separate python model script, ex. after importing Models_2D, calling Models_2D.no_mig
+    func: access the model function from within 'dadi_Run_Optimizations.py' or from a separate python model script, ex. after importing Models_2D, calling Models_2D.no_mig
     rounds: number of optimization rounds to perform
     param_number: number of parameters in the model selected (can count in params line for the model)
 
