168c9205d4fdf88202bf2d480db7ed17d2e8696a
dportik <daniel.portik@gmail.com>
2019-10-03 11:35:27 -0700

    catch more input errors

--- Two_Population_Pipeline/Summarize_Outputs.py
+++ Two_Population_Pipeline/Summarize_Outputs.py
@@ -38,25 +38,44 @@ import os
 file_dir = sys.argv[1]
 os.chdir(file_dir)
 
+#check if summary files already exist in output directory
+results = sorted([os.path.abspath(f) for f in os.listdir(file_dir) if f.startswith("Results_Summary")])
+if results:
+    raise ValueError("\n\n\nWARNING: Summary files are already located in the directory specified."
+                         " Please remove them before running this script.\n\n")
+    
+
 #initiate empty lists that we will fill with summary information
 summary_list = []
 simple_list = []
 
 #list comprehension to find output files
-flist = sorted([f for f in os.listdir('.') if f.endswith(".optimized.txt")])
-print("\n\nFound {} output files to summarize.\n".format(len(flist)))
+flist = sorted([os.path.abspath(f) for f in os.listdir(file_dir) if f.endswith("optimized.txt")])
+
+#do tasks depending on whether files located or not
+if flist:
+    print("\n\nFound {} output files to summarize:".format(len(flist)))
+    for f in flist:
+        print("\t{}".format(f.split('/')[-1]))
+else:
+    print("\n\nFound {} output files to summarize!\n\n".format(len(flist)))
+    print("Please check to make sure the output files end with '.optimized.txt' and are"
+              "located in the directory specified:\n\t{}".format(file_dir))
 
 #iterate over output files
 for f in flist:
-    content = []
-    print("\tExtracting contents from: {}".format(f))
-    #open file, skip first line
-    with open(f, 'r') as fh:
-        next(fh)
-        #split lines and add contents to list
-        for line in fh:
-            content.append(line.strip().split('\t'))
+    print("\nExtracting contents from: {}".format(f.split('/')[-1]))
     #content list items will have order: "Model"	"Replicate"	"log-likelihood"	"AIC"	"chi-squared"	"theta"	"optimized_params(xxx)"
+    with open(f, 'r') as fh:
+        lines = [line.strip().split('\t') for line in fh if not line.startswith("Model")]
+    #strict filtering: sublists must have 7 elements and not contain any "nan" entries
+    content = [l for l in lines if len(l) == 7 and "nan" not in l]
+    if len(lines) == len(content):
+        print("\tFound {} total replicates.".format(len(lines)))
+    else:
+        print("\tFound {} row entries.".format(len(lines)))
+        print("\tRemoved {} row entries due to presence of 'nan' values or incomplete data.".format(len(lines)-len(content)))
+        
     #let's sort all the rows by AIC, lowest to highest
     content.sort(key=lambda x: float(x[3]))
         
@@ -67,27 +86,33 @@ for f in flist:
     #add top entry to easy list
     simple_list.append(content[0])
    
-#sort the list containing only the top entry for each model by order of AIC
-simple_list.sort(key=lambda x: float(x[3]))
+#make sure results are actually in lists
+if simple_list and summary_list:
+    
+    #sort the list containing only the top entry for each model by order of AIC
+    simple_list.sort(key=lambda x: float(x[3]))
 
-#create output file 1
-out1 = "Results_Summary_Extended.txt"
-with open(out1, 'a') as fh:
-    #write tab-delimited file with extended results
-    fh.write("Model\tReplicate\tlog-likelihood\tAIC\tchi-squared\ttheta\toptimized_params\n")
-    for row in summary_list:
-        for val in row:
-            fh.write("{}\t".format(val))
-        fh.write("\n")
+    #create output file 1
+    out1 = "Results_Summary_Extended.txt"
+    with open(out1, 'a') as fh:
+        #write tab-delimited file with extended results
+        fh.write("Model\tReplicate\tlog-likelihood\tAIC\tchi-squared\ttheta\toptimized_params\n")
+        for row in summary_list:
+            for val in row:
+                fh.write("{}\t".format(val))
+            fh.write("\n")
 
-#create output file 2
-out2 = "Results_Summary_Short.txt"
-with open(out2, 'a') as fh:
-    #write tab-delimited file with simplified results
-    fh.write("Model\tReplicate\tlog-likelihood\tAIC\tchi-squared\ttheta\toptimized_params\n")
-    for row in simple_list:
-        for val in row:
-            fh.write("{}\t".format(val))
-        fh.write("\n")
+    #create output file 2
+    out2 = "Results_Summary_Short.txt"
+    with open(out2, 'a') as fh:
+        #write tab-delimited file with simplified results
+        fh.write("Model\tReplicate\tlog-likelihood\tAIC\tchi-squared\ttheta\toptimized_params\n")
+        for row in simple_list:
+            for val in row:
+                fh.write("{}\t".format(val))
+            fh.write("\n")
 
-print("\n\nSummary files '{0}' and '{1}' have been written to: \n\t{2}\n\n".format(out1, out2, file_dir))
+    print("\n\nSummary files '{0}' and '{1}' have been written to: \n\t{2}\n\n".format(out1, out2, file_dir))
+
+else:
+    print("\n\nNo results were written!\n\n")
